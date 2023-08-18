147cffd7bacd3ee065c51294abe2fd0e3208ffca
joshuaipwork <joshuaipwork@gmail.com>
2023-08-17 20:26:49 -0700

    Update start to use token in yaml

--- start.py
+++ start.py
@@ -1,16 +1,15 @@
 import discord
-import argparse
-import json
+import yaml
 from SyntheaClient import SyntheaClient
 
-with open('secrets.json', "r") as f:
-    secrets = json.load(f)
+with open("config.yaml", "r", encoding="utf-8") as file:
+    config = yaml.safe_load(file)
 
-    # set up the discord bot
-    intents = discord.Intents.all()
-    intents.message_content = True
-    intents.presences = True
-    intents.members = True
+# set up the discord bot
+intents = discord.Intents.all()
+intents.message_content = True
+intents.presences = True
+intents.members = True
 
-    client = SyntheaClient(intents=intents)
-    client.run(secrets['client_token'])
+client = SyntheaClient(intents=intents)
+client.run(config["client_token"])
