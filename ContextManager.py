901e49765de12a14c6a667399f68c38d4e8bbb0a
joshuaipwork <joshuaipwork@gmail.com>
2023-08-20 21:56:40 -0700

    Reformat contextManager, update to use context_length instead of seqlen

--- ContextManager.py
+++ ContextManager.py
@@ -10,12 +10,14 @@ from SyntheaModel import ChattyModel
 
 from CommandParser import ChatbotParser, CommandError, ParserExitedException
 
+
 class ThreadHistoryIterator:
     """
     An async iterator which follow replies in a thread until it reaches the oldest message
     in the thread. Unlike discord.Thread.history(), this iterator will parse commands into
 
     """
+
     def __init__(self, starting_message: discord.Message):
         self.message = starting_message
         self.thread: discord.Thread = self.message.channel
@@ -23,7 +25,7 @@ class ThreadHistoryIterator:
 
     def __aiter__(self):
         return self
-    
+
     async def __anext__(self):
         # if we have already reached the beginning of the thread, there are no further messages to read
         if self.message.id == self.thread.id:
@@ -39,22 +41,26 @@ class ThreadHistoryIterator:
             # pylint: disable-next=raise-missing-from
             raise StopAsyncIteration
 
+
 class ReplyChainIterator:
     """
     An async iterator which follows a chain of discord message replies until it reaches the end
     or fails to capture the last message.
     """
+
     def __init__(self, starting_message: discord.Message):
         self.message = starting_message
 
     def __aiter__(self):
         return self
-    
+
     async def __anext__(self):
         # go back message-by-message through the reply chain and add it to the context
         if self.message.reference:
             try:
-                self.message = await self.message.channel.fetch_message(self.message.reference.message_id)
+                self.message = await self.message.channel.fetch_message(
+                    self.message.reference.message_id
+                )
                 return self.message
 
             except (discord.NotFound, discord.HTTPException, discord.Forbidden):
@@ -65,38 +71,33 @@ class ReplyChainIterator:
         else:
             raise StopAsyncIteration
 
+
 class ContextManager:
     """
     Formats prompts for the bot to generate from.
     """
+
     # A rough measure of how many character are in each token.
     EST_CHARS_PER_TOKEN = 3
 
     def __init__(self, model: ChattyModel, bot_user_id: int):
-        """        
+        """
         model (str): The model that is generating the text. Used to determine the prompt format
             and other configuration options.
         bot_user_id (str): The discord user id of the bot. Used to determine if a message came from
             the bot or from a user.
         """
         self.model: ChattyModel = model
-        self.seq_len: int = self.model.config['seqlen']
-        # # used to calculate how much context can be included in a message.
-        # self.max_new_tokens: int = self.model.config['max_new_tokens']
-        # # self.user_message_tag: str = self.model.format['user_message_tag']
-        # # self.bot_message_tag: str = self.model.format['bot_message_tag']
-        # # self.system_prompt: str = self.model.format['system_prompt']
+        self.context_length: int = self.model.config["context_length"]
         self.bot_user_id: int = bot_user_id
 
         # TODO: Split the model config and the general bot config.
-        with open('config.yaml', "r", encoding="utf-8") as file:
+        with open("config.yaml", "r", encoding="utf-8") as file:
             self.config = yaml.safe_load(file)
 
     async def compile_prompt_from_chat(
-            self,
-            message: discord.Message,
-            character: Optional[str]=None
-        ):
+        self, message: discord.Message, character: Optional[str] = None
+    ):
         """
         Generates a prompt which includes the context from previous messages in a reply chain.
         Messages outside of the reply chain are ignored.
@@ -113,13 +114,13 @@ class ContextManager:
             character=character,
         )
         return final_prompt
-        
+
     async def compile_prompt(
-            self,
-            message: discord.Message,
-            history_iterator: AsyncIterator[discord.Message],
-            character: Optional[str]=None
-        ):
+        self,
+        message: discord.Message,
+        history_iterator: AsyncIterator[discord.Message],
+        character: Optional[str] = None,
+    ):
         """
         Generates a prompt which includes the context from previous messages from the history.
 
@@ -135,14 +136,14 @@ class ContextManager:
         token_count: int = 0
 
         system_prompt: str = ""
-        bot_message_tag = self.model.format['bot_message_tag']
-        system_prompt = self.config['system_prompt']
+        bot_message_tag = self.model.format["bot_message_tag"]
+        system_prompt = self.config["system_prompt"]
 
         # if the character has a special system prompt, override the default model system prompt
         if character:
-            with open(f"characters/{character}.yaml", 'r', encoding='utf-8') as f:
+            with open(f"characters/{character}.yaml", "r", encoding="utf-8") as f:
                 char_chat_config = yaml.safe_load(f)
-                system_prompt = char_chat_config['system_prompt']
+                system_prompt = char_chat_config["system_prompt"]
 
         # add the bot role tag so the AI knows to continue from this point.
         history.append(f"{bot_message_tag} ")
@@ -150,10 +151,14 @@ class ContextManager:
 
         # history contains the most recent messages that were sent before the current message.
         system_prompt_tokens: int = len(system_prompt) // self.EST_CHARS_PER_TOKEN
-        history_token_limit: int = self.seq_len - self.model.config['max_new_tokens'] - system_prompt_tokens
+        history_token_limit: int = (
+            self.context_length
+            - self.model.config["max_new_tokens"]
+            - system_prompt_tokens
+        )
 
         # add the user's last message
-        user_tag: str = self.model.format['user_message_tag']
+        user_tag: str = self.model.format["user_message_tag"]
         text, added_tokens = self._get_text(message)
         history.append(self._form_message(user_tag, text))
         token_count += added_tokens
@@ -162,7 +167,7 @@ class ContextManager:
             history,
             token_count=token_count,
             token_limit=history_token_limit,
-            history_iterator=history_iterator
+            history_iterator=history_iterator,
         )
 
         # add the system prompt to the beginning of the prompt
@@ -181,15 +186,15 @@ class ContextManager:
         Args:
             system_prompts (str): The system prompt to use for the header.
         """
-        template = self.model.format['header_template']
-        result = template.replace('<|system_prompt|>', system_prompt)
+        template = self.model.format["header_template"]
+        result = template.replace("<|system_prompt|>", system_prompt)
 
         return result
 
     def _get_user_tag(self, message: discord.Message) -> str:
         """
         Returns the name of the user who sent this message.
-        
+
         Args:
             message (discord.Message): The message to retrieve the name of
         Returns:
@@ -208,7 +213,7 @@ class ContextManager:
         elif message.author.name:
             user_tag = message.author.name
         else:
-            user_tag = 'user'
+            user_tag = "user"
 
         return user_tag
 
@@ -220,20 +225,19 @@ class ContextManager:
                 from the prompt format and the identity of the user.
             text (str): The text of the message.
         """
-        template: str = self.model.format['message_template']
+        template: str = self.model.format["message_template"]
         template = template.replace("<|message_tag|>", message_tag)
         result = template.replace("<|text|>", text)
 
         return result
 
     async def _follow_history(
-            self,
-            history: list[str],
-            token_limit: int,
-            history_iterator: AsyncIterator[discord.Message],
-            token_count: int = 0,
-
-        ):
+        self,
+        history: list[str],
+        token_limit: int,
+        history_iterator: AsyncIterator[discord.Message],
+        token_count: int = 0,
+    ):
         """
         Iterates through history_iterator, retrieving messages until either
         the context length is exhausted or we run out of messages.
@@ -257,9 +261,13 @@ class ContextManager:
 
             # update the prompt with this message
             if message.author.id == self.bot_user_id:
-                history.append(self._form_message(self.model.format['bot_message_tag'], text))
+                history.append(
+                    self._form_message(self.model.format["bot_message_tag"], text)
+                )
             else:
-                history.append(self._form_message(self.model.format['user_message_tag'], text))
+                history.append(
+                    self._form_message(self.model.format["user_message_tag"], text)
+                )
 
             # TODO: Figure out how to do multi-character conversations. Looks like not every model works well with that...
             # if message.embeds:
@@ -270,7 +278,7 @@ class ContextManager:
             # else:
             #     user_tag: str = self._get_user_tag(message)
             #     history.append(f"{user_tag.upper()}: {text}")
-        
+
         return history
 
     def _get_text(self, message: discord.Message):
@@ -283,7 +291,7 @@ class ContextManager:
         # when the bot plays characters, it stores text in embeds rather than content
         if message.author.id == self.bot_user_id and message.embeds:
             text = message.embeds[0].description
-        elif message.clean_content.startswith(self.config['command_start_str']):
+        elif message.clean_content.startswith(self.config["command_start_str"]):
             try:
                 args = ChatbotParser().parse(message.clean_content)
                 text = args.prompt
@@ -295,5 +303,3 @@ class ContextManager:
 
         tokens = len(text) // self.EST_CHARS_PER_TOKEN
         return text, tokens
-
-    
