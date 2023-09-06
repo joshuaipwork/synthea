914b36bfaa583ab5c72864bf5e0204fc29ffcb6e
joshuaipwork <joshuaipwork@gmail.com>
2023-09-06 09:21:32 -0700

    Update requirements to include the synthea module.

--- install.sh
+++ install.sh
@@ -10,4 +10,6 @@ conda create -y --name "$CONDA_ENV_NAME" python=3.11
 conda activate "$CONDA_ENV_NAME"
 pip install -r requirements.txt
 
+# TODO: Add logic for modifying CMAKE args based on GPU backend. CLI input?
 CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12/bin/nvcc" FORCE_CMAKE=1 pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
+pip install -e .
