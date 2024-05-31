ffc0536696ced2893da478f94382b49f0ee8d71a
joshuaipwork <joshuaipwork@gmail.com>
2024-05-31 01:45:25 -0700

    misc: apply config updates

--- install.sh
+++ install.sh
@@ -3,14 +3,8 @@
 # import the version number 
 VERSION=$(cat ./version.txt)
 CONDA_ENV_NAME="synthea_$VERSION"
-
 echo "Installing Synthea version $VERSION"
 conda create -y --name "$CONDA_ENV_NAME" python=3.11
-
 conda activate "$CONDA_ENV_NAME"
 pip install -r requirements.txt
-
-# TODO: Add logic for modifying CMAKE args based on GPU backend. CLI input?
-sudo apt install cmake -y
-CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12/bin/nvcc" FORCE_CMAKE=1 pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
 pip install -e .
